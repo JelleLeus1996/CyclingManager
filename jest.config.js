1ef9f6478c0db4fa281003c3b451eb6e409bf6e4
JelleLeus1996 <101363492+JelleLeus1996@users.noreply.github.com>
2024-08-16 12:42:20 +0000

    adjustment jest & test

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,31 @@
 "use strict";
 /**
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
+* For a detailed explanation regarding each configuration property, visit:
+* https://jestjs.io/docs/configuration
+*/
 Object.defineProperty(exports, "__esModule", { value: true });
 /** @type {import('jest').Config} */
 const config = {
-    collectCoverageFrom: [
-        './src/entities/*.ts',
-        './src/services/*.ts',
-        './src/models/*.ts',
-        './src/controllers/*.ts'
-    ],
-    coverageDirectory: '__tests__/coverage',
-
-    coverageProvider: 'v8',
-
-    globalSetup: './__tests__/global.setup.ts',
-    // A path to a module which exports an async function that is triggered once after all test suites
-    globalTeardown: './__tests__/global.teardown.ts',
-
-    testMatch: [
-        '**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)',
-    ],
-   
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    verbose: true,
-    forceExit: true,
+  collectCoverageFrom: [
+    "./src/entities/*.ts",
+    "./src/services/*.ts",
+    "./src/models/*.ts",
+    "./src/controllers/*.ts",
+  ],
+  coverageDirectory: "__tests__/coverage",
+ 
+  coverageProvider: "v8",
+ 
+  globalSetup: "./src/__tests__/global.setup.ts",
+  // A path to a module which exports an async function that is triggered once after all test suites
+  globalTeardown: "./src/__tests__/global.teardown.ts",
+ 
+  testMatch: ["**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)"],
+ 
+  preset: "ts-jest",
+  testEnvironment: "node",
+  verbose: true,
+  forceExit: true,
 };
 exports.default = config;
 //# sourceMappingURL=jest.config.js.map
--- src/__tests__/supertest.setup.ts
+++ src/__tests__/supertest.setup.ts
@@ -1,49 +1,57 @@
-import supertest from 'supertest';
+import supertest from "supertest";
 import Koa from "koa";
 import { AppDataSource } from "../core/connection_tests";
-import createServer from '../createServer'
+import createServer from "../createServer";
 const app = createServer();
-
+ 
 export const login = async (): Promise<string> => {
-  const response = await supertest(app.)
+  const response = await supertest
+    .agent(app.callback())
     .post("/api/users/login")
     .send({
       email: "Magnus.Bäckstedt@hotmail.com",
       password: "12345678",
     })
     .set("Accept", "application/json");
-
+ 
   if (response.statusCode !== 200) {
     throw new Error(response.body.message || "Unknown error occurred");
   }
-
+ 
   return `Bearer ${response.body.token}`;
 };
-
-export const loginAdmin = async (): Promise<string> =>  {
-  const response = await supertest.post("/api/users/login").send({
-    email: "jelle.leus@student.hogent.be",
-    password: "12345678",
-  });
-
+ 
+export const loginAdmin = async (): Promise<string> => {
+  const response = await supertest(app.callback())
+    .post("/api/users/login")
+    .send({
+      email: "jelle.leus@student.hogent.be",
+      password: "12345678",
+    });
+ 
   if (response.statusCode !== 200) {
     throw new Error(response.body.message || "Unknown error occurred");
   }
-
+ 
   return `Bearer ${response.body.token}`;
 };
-
-export const withServer = (setter: (arg: { request: supertest.SuperTest<supertest.Test> }) => void): void => {
+ 
+export const withServer = (
+  setter: (arg: { request: supertest.SuperTest<supertest.Test> }) => void
+): void => {
   let app;
-
+ 
   beforeAll(async () => {
     await AppDataSource.initialize();
-    app = await createServer();
-    const request = supertest(app.callback());
+    app = createServer();
+    const request = supertest(
+      app.callback()
+    ) as unknown as supertest.SuperTest<supertest.Test>; // Cast the request object to the correct type
     setter({ request });
   });
-
+ 
   afterAll(async () => {
     await AppDataSource.destroy();
   });
 };
+ 
